29d42f7e7b9845a4169d0878827230893c729ffc
lebefriedlich <noval.akbar.906@gmail.com>
2025-06-21 18:21:46 +0700

    Repair error in migration

--- database/migrations/2025_06_21_054702_create_iaqi_table.php
+++ database/migrations/2025_06_21_054702_create_iaqi_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('iaqi', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->foreignUuid('region_id')->constrained('regions')->onDelete('cascade');
             $table->timestamp('observed_at')->index();
             $table->string('dominent_pol');
